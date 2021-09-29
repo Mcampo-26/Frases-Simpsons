e2ad21faa30660237837b3ece6af7847ed981ca0
Mauricio Campo <mcampo26@gmail.com>
2021-09-29 10:06:22 -0500

    se garego la logica para obtener los datos y mostrarla en lel componente frase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+import Frase from './components/Frase';
+import {useState, useEffect} from "react"
 
 function App() {
+  // creo el state 
+  const [personaje,setPersonaje]=useState({});
+  useEffect(()=>{
+    // aqui escribo la logica
+    consultaApi()
+  },[]);// los parentesis al final es para que cargue en el montaje
+
+  const consultaApi = async()=>{
+    const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
+    const resultado = await respuesta.json();
+    console.log(resultado[0]);
+    //guardar los dato en el state
+    setPersonaje(resultado[0]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <section className="container my-5 d-flex flex-column align-items-center">
+    <img src={process.env.PUBLIC_URL+"Logo3.png"} alt="Logo Simpsons" className="w-75"/>
+    <Button variant="warning" onClick={()=>consultaApi()} >Obtener frase</Button>
+    <Frase personajeProp={personaje}></Frase>
+    </section>
+
   );
 }
 
